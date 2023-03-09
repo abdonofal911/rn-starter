dae95b1fc0f0c35b265905904649288a1604a99a
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-03-09 12:37:36 +0200

    Changes to be committed: 	modified:   App.js     new file:   src/screens/ColorScreen.js 	new file:   src/screens/CounterScreen.js 	modified:   src/screens/HomeScreen.js
      Changes not staged for commit:
    	modified:   App.js
    	modified:   src/screens/HomeScreen.js
    
     Untracked files:
    	src/Components/ColoCounter.js
    	src/screens/SquareScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,42 +1,50 @@
 import React from "react";
-import { Text, StyleSheet ,Button , TouchableOpacity, View} from "react-native";
+import { Text, StyleSheet, Button, TouchableOpacity, View } from "react-native";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
-    <Text style={styles.text} >
-    Welcome at Home
-    </Text>
+      <Text style={styles.text}>Welcome at Home</Text>
 
-    <Button 
-    style={styles.Button}
-    onPress={()=>navigation.navigate('Components')}
-    title="Go to Components" />
+      <Button
+        style={styles.Button}
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components"
+      />
 
-    <Button 
-    style={styles.Button}
-    onPress={()=>navigation.navigate('List')}
-    title="Go to List" />
-   
-    
-    <Button 
-    style={styles.Button}
-    onPress={()=>navigation.navigate('Images')}
-    title="Go to Image Screen" />
-   
-    
+      <Button
+        style={styles.Button}
+        onPress={() => navigation.navigate("List")}
+        title="Go to List"
+      />
+
+      <Button
+        style={styles.Button}
+        onPress={() => navigation.navigate("Images")}
+        title="Go to Image Screen"
+      />
+
+      <Button
+        style={styles.Button}
+        onPress={() => navigation.navigate("Counter")}
+        title="Go to Counter Screen"
+      />
+      <Button
+        style={styles.Button}
+        onPress={() => navigation.navigate("Color")}
+        title="Go to Color Screen"
+      />
     </View>
-    );
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
-    
   },
-  Button:{
-    marginVertical:50
-  }
+  Button: {
+    marginVertical: 50,
+  },
 });
 
 export default HomeScreen;
